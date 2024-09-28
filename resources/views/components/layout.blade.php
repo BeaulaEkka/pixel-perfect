dcd7f747d3f9d726abf77514a26303cad41d0196
Your Name <ekka.beaula@gmail.com>
2024-09-28 09:19:41 +0200

    welcome page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,18 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Perfect</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js'])
 </head>
 
 <body class="">
     <div>
-        <nav class=" w-full h-20  shadow-md flex justify-between r">
-            <div class="w-[80%] mx-auto"><a href=""><img class="w-40"
-                        src={{ Vite::asset('resources/images/logo.png') }} alt=""></a></div>
-            <div>links</div>
-            <div>post a job</div>
+        <nav class="flex items-center w-full h-16 p-4 shadow-sm lg-p-10 ">
+            <div class="w-full lg:w-[80%] mx-auto flex items-center justify-between ">
+                <a href="/">
+                    <!-- Corrected image path with Vite handling -->
+                    <img class="w-40" src="{{ Vite::asset('resources/images/logo.png') }}" alt="Logo">
+                </a>
+
+                <div class="flex space-x-2 font-semibold capitalize lg:gap-8 "><a href="">Jobs</a>
+                    <a href="">Careers</a>
+                    <a href="">Salaries</a>
+                    <a href="">Companies</a>
+                </div>
+                <div class="capitalize"><a href="">post a job</a></div>
+            </div>
         </nav>
-        <main>{{ $slot }}</main>
+        <main class="mt-10 max-w-[80%] mx-auto ">{{ $slot }}</main>
     </div>
 
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    welcome
+    <section>
+        <h3>Top Jobs</h3>
+        <div>
+            <div class="capitalize">laracast</div>
+            <div>
+                <div>Video Producer</div>
+                <div>Full-Time From $60,000</div>
+
+            </div>
+        </div>
+    </section>
 </x-layout>
